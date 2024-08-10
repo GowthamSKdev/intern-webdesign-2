3eeee105baec6661c23867f4a09f6b52fbe23460
GowthamSKdev <gowthams2647@gmail.com>
2024-08-10 13:49:15 +0530

    add navbar style and write side-navbar & side-navbar-js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var side_menu = document.querySelector(".side_nav_menu");
+function open_side_menu() {
+  side_menu.style.display = "flex";
+}
+function close_side_menu() {
+  side_menu.style.display = "none";
+}
